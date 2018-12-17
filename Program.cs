4aba6455b0cb212aa6bbfa143e1df87445366474
BilalyC <bilaly.camara@outlook.com>
2018-12-17 16:36:43 +0100

    Exercice 4 Complet

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string civilite, nom;
+            int nb;
+
+            civilite = "Monsieur";
+            nom = "Vincent";
+            nb = 1;
+
+            //Console.WriteLine($"Bonjour {civilite} {nom}, vous êtes venu nous rendre visite {nb} fois");
+            Console.WriteLine("Bonjour " + civilite + " " + nom + ", vous êtes venu nous rendre visite " + nb + " fois");
+        }
+    }
+}
